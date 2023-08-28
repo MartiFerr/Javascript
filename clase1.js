9e7b5958c4731c5c0718515a9c688a471001b421
alumno <alumno@Instituto.local>
2023-08-28 10:38:48 -0300

    ccc

---
+++ clase1.js
@@ -0,0 +1,43 @@
+
+
+--function multiplicacion ( a, b)
+{
+let multiplicar = a * b
+return multiplicar
+}
+alert (multiplicacion( 10, 5))
+
+function suma ( c, d)
+{
+    let suma = c + d 
+    return suma
+}
+alert (multiplicacion(66, 70))
+
+function restar ( e, f)
+{
+    let resta = e - f
+    return resta 
+}
+alert (restar(130, 40))
+
+function dividir ( g, h)
+{
+    let divicion = 58 / 4
+    return  divicion
+}
+alert (dividir( 60, 3))
+
+function sumaypotencia (x, b)
+{
+    let resultado = x * x + b
+    return resultado
+}
+alert (sumaypotencia(6, 7))
+
+function Saludar (Nombre,Apellido)
+{
+    let Nombre = 'Marti'
+    return 'Bienvenido/a' +  Nombre + Apellido
+}
+Saludar( 'Clara',)
