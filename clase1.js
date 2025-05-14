2cf65d6e351f62243d1c9f45f6866bd99d2e65d9
alumno <alumno@Instituto.local>
2025-05-14 08:17:32 -0300

    cambios

--- clase1.js
+++ clase1.js
@@ -1,60 +1,27 @@
+Arrays: Operaciones
 
+let colores = ["Blanco", "Negro", "Azul", "Rosa", "Rojo"];
+let primero = colores[0];
+let ultimo = colores[colores.length - 1];
+console.log(colores.length); 
+frutas.push("Celeste"); 
+frutas.unshift("Gris");
+frutas.pop();
+frutas.shift();
+colores[1] = "Amarillo";
+array1.forEach((frutas) => console.log(frutas));
+let nuevosColores = [];
 
---function multiplicacion ( a, b)
 {
-let multiplicar = a * b
-return multiplicar
+  let color = prompt(`Ingresa el color`);
+  nuevosColores.push(color);
 }
-alert (multiplicacion( 10, 5))
 
-function suma ( c, d)
-{
-    let suma = c + d 
-    return suma
-}
-alert (multiplicacion(66, 70))
+console.log("Colores ingresados:", nuevosColores);
 
-function restar ( e, f)
-{
-    let resta = e - f
-    return resta 
-}
-alert (restar(130, 40))
 
-function dividir ( g, h)
-{
-    let divicion = 58 / 4
-    return  divicion
-}
-alert (dividir( 60, 3))
 
-function sumaypotencia (x, b)
-{
-    let resultado = x * x + b
-    return resultado
-}
-alert (sumaypotencia(6, 7))
 
-function Saludar (nombre, apellido)
-{
-return  'bienvenido' + nombre + apellido
-}
-alert(Saludar(Martina, Ferrando)) 
 
-function operacion (x)
-{
-    let resultado = cuadrado (x) + 5
-    return resultado
-cuadrado (100)
 
-/* 1) Crear ubna funcion que calcule el cuadrado de un numero
-2) Crear una funciom que calcule el cubo de un numero 
-3) Crer una funcion por cada de las siguientes operaciones:
-a) x^2-x-10
-b) (x-3)*2
-c)x*x^2+20
-d)(x/2)+5
-e) x^3+x^2+10
-f)(x*10)*5
 
-*/
