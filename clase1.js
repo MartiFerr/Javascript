85c6cc624c76d018fb91d866bd4cb094d166b832
alumno <alumno@Instituto.local>
2025-05-15 11:16:09 -0300

    cambio

--- clase1.js
+++ clase1.js
@@ -1,17 +1,32 @@
 Arrays: Operaciones
 
-let colores = ["Blanco", "Negro", "Azul", "Rosa", "Rojo"];
+//1 Creación de un arreglo
+let colores = ["Blanco", "Negro", "Azul", "Rosa", "Rojo"]
+//2 Acceso a elementos
 let primero = colores[0];
-let ultimo = colores[colores.length - 1];
-console.log(colores.length); 
-colores.push("Celeste"); 
-colores.unshift("Gris");
-colores.pop();
-colores.shift();
-colores[1] = "Amarillo";
-array1.forEach((colores) => console.log(colroes));
+console.log ("Primer Elemento" + colores[0])
+console.log("Ultimo Elemeto" + colores[colores.length - 1])
+//3 Tamaño del arreglo
+console.log ("El tamaño del arreglo es" + [colores.length - 1])
+//4 Agregar al final
+colores.push("Celeste")
+console.log (colores [colores.length - 1])
+//5 Agregar al inicio
+colores.unshift("Gris")
+console.log (colores[0])
+//6 Eliminar del final
+colores.pop()
+console.log (colores)
+//7 Eliminar del principio
+colores.shift()
+console.log (colores)
+//8 Reemplazo de un valor
+colores[1] = "Amarillo"
+console.log (colores)
+//9 Recorrido del arreglo
+array1.forEach((colores) => console.log(colores));
 let nuevosColores = [];
-
+//10 Desafío creativo
 {
   let color = prompt(`Ingresa el color`);
   nuevosColores.push(color);
